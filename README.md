c0c17b8d1564ad9e502841e47ea1f47b15fdc0ab
Miran <miran.loncaric@gmail.com>
2014-01-25 17:03:53 +0100

    ..

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 teleport-starter
 ================
 
-After cloning execute
+Clone a folder
+`git clone https://github.com/mloncaric/teleport-starter.git`
+
+Rename cloned folder
+`mv teleport-starter custom-folder-name`
+
+cd into it
+`cd custom-folder-name`
+
+Remove .git folder
+`rm -Rf .git`
+
+Add <a href="https://github.com/mloncaric/teleport" target="_blank">teleport package</a> and its dependencies.
 `mrt add teleport`
-which will then install the <a href="https://github.com/mloncaric/teleport" target="_blank">teleport package</a> and its dependencies.
 
 Enter `./run` to start the server
 
--- client/application/main.js
+++ client/application/main.js
@@ -1,1 +1,39 @@
-applicationView = new Teleport.View("application");
+applicationView = new Teleport.View("application", {
+	// Executes when template gets rendered for the first time
+	show: function() {
+		
+	},
+	
+	// Executes when template gets destroyed
+	hide: function() {
+		
+	},
+	
+	// Hook shared objects to the view's lifecycle
+	sharedObjects: function() {
+		return [
+			sharedObject
+		];
+	}
+});
+
+// Temporary, will be added to the view options
+// Executes every time a template is rendered
+Template.application.rendered = function() {
+	
+}
+
+// Shares anything between users in current session, it's also persisted
+var sharedObject = new Teleport.SharedObject("sharedObject", {
+	// Called when user calls reset method on SharedObject instance
+	// Also called when session starts (ie the object has no data yet)
+	reset: function() {
+		sharedObject.set("key", "initial value");
+		// sharedObject.setMany({a: 1, b: "2", c: true, d: []});
+	},
+	
+	// Called when someone calls set or setMany method
+	sync: function() {
+		console.log("sharedObject sync:", sharedObject.get("key"));
+	}
+});
